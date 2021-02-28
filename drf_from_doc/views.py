3f48bf5dbd3f44b08107084eeccf599acc35cdfa
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-02-28 12:16:47 +0600

    Update drf_from_doc views

--- drf_from_doc/views.py
+++ drf_from_doc/views.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.models import Group, User
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
-from drf_from_doc.serializers import UserSerializer, GroupSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import permissions, viewsets
+from rest_framework.parsers import JSONParser
+
+from .serializers import GroupSerializer, UserSerializer, SnippetSerializer
+
+from .models import Snippet
+
 # Create your views here.
 
 
@@ -22,3 +28,57 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    # Fetch data from database and show
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    # Create: fetch data from client site and store in database
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            # 400 Bad Request response status code indicates that the server cannot or will not process the request due to something that is perceived to be a client error
+            return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        # 404 message it means that the requested website information was not found on the server.should check whether the address was written correctly in the address bar.
+        return HttpResponse(status=404)
+
+    # show details
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    # Update
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+
+        return JsonResponse(serializer.errors, status=400)
+
+    # Delete
+    elif request.method == 'DELETE':
+        snippet.delete()
+        #HTTP 204 No Content success status response code indicates that a request has succeeded, but that the client doesn't need to navigate away from its current page.
+        return HttpResponse(status=204)
