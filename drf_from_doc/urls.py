0f4ce2dcdff580a6c6d28306c6439e5a7f10a743
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-02-28 12:45:42 +0600

    snippet urls is ok

--- drf_from_doc/urls.py
+++ drf_from_doc/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from drf_from_doc import views
+from . import views
 
 router = routers.DefaultRouter()
 router.register('users', views.UserViewSet)
@@ -8,5 +8,8 @@ router.register('groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>', views.snippet_detail),
 ]
