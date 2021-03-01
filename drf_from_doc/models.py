b55399d6bfea994dfd8dbdb9d3b8d867abbf9c9b
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-03-01 13:33:57 +0600

    update drf_from_doc
    updated models, serializers, urls, views

--- drf_from_doc/models.py
+++ drf_from_doc/models.py
@@ -20,11 +20,11 @@ class Snippet(models.Model):
     style = models.CharField(choices=STYLE_CHOICES,
                              default='friendly', max_length=100)
     owner = models.ForeignKey(
-        'auth.User', related_name='snippet', on_delete=models.CASCADE)
+        'auth.User', related_name='snippets', on_delete=models.CASCADE)
     highlighted = models.TextField()
 
     class Meta:
-        ordering = ['created']
+        ordering = ['created', ]
 
     def save(self, *args, **kwargs):
         """
--- drf_from_doc/urls.py
+++ drf_from_doc/urls.py
@@ -3,12 +3,12 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('users', views.UserViewSet)
-router.register('groups', views.GroupViewSet)
+router.register('users', views.QuickUserViewSet)
+router.register('groups', views.QuickGroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>', views.snippet_detail),
