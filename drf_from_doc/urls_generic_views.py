8d383c1e1b8913e577fb7f498906db1f3a5ab11f
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-03-03 22:48:19 +0600

    add comments

--- drf_from_doc/urls_generic_views.py
+++ drf_from_doc/urls_generic_views.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views_generic_views
-
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 urlpatterns = [
     path('userlist/', views_generic_views.UserList.as_view()),
+    # OR. same thing we can do it in one line
+    path('users/', generics.ListCreateAPIView.as_view(queryset=User.objects.all(),
+                                                      serializer_class=UserSerializer), name='user-list'),
     path('userlistcustom/', views_generic_views.UserListCustom.as_view()),
 ]
